0c5f26d946bebfdd0132ed362907633b49c5c5bf
Hyago Andrade <hyagosdj@gmail.com>
2024-05-22 21:56:07 -0300

    fix: not convert float to string

--- main.py
+++ main.py
@@ -17,37 +17,44 @@ while True:
   opcao = input(menu).upper()
 
   if opcao == "D":
-    valor = float(input("Informe o valor do depósito: "))
-    if valor > 0:
-      saldo += valor  # type: ignore
-      extrato += f"Depósito: R$ {valor:.2f}\n"
+    try:
+      valor = float(input("Informe o valor do depósito: "))
+    
+      if valor > 0:
+        saldo += valor  # type: ignore
+        extrato += f"Depósito: R$ {valor:.2f}\n"
 
-    else:
-      print("Operação falhou! O valor informado é inválido.")
+      else:
+        print("Operação falhou! O valor informado é inválido.")
+    except:  # noqa: E722
+      print("Algo deu errado! Só aceita números.")
 
   elif opcao == "S":
-    valor = float(input("Informe o valor do saque: "))
-    
-    excedeu_saldo = valor > saldo
-    excedeu_limite = valor > limite
-    excedeu_saques = numero_saques >= LIMITE_SAQUES
+    try:
+      valor = float(input("Informe o valor do saque: "))
+      
+      excedeu_saldo = valor > saldo
+      excedeu_limite = valor > limite
+      excedeu_saques = numero_saques >= LIMITE_SAQUES
 
-    if excedeu_saldo:
-      print("Operação falhou! Você não tem saldo suficiente.")
-    
-    elif excedeu_limite:
-      print("Operação falhou! O valor do saque excede o limite.")
-    
-    elif excedeu_saques:
-      print("Operação falhou! Número máximo de saques excedido.")
+      if excedeu_saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+      
+      elif excedeu_limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+      
+      elif excedeu_saques:
+        print("Operação falhou! Número máximo de saques excedido.")
 
-    elif valor > 0:
-      saldo -= valor  # type: ignore
-      extrato += f"Saque: R$ {valor:.2f}\n"
-      numero_saques += 1
-    
-    else:
-      print("Operação falhou! O valor informado é inválido.")
+      elif valor > 0:
+        saldo -= valor  # type: ignore
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+      
+      else:
+        print("Operação falhou! O valor informado é inválido.")
+    except:  # noqa: E722
+      print("Algo deu errado! Só aceita números.")
 
   elif opcao == "E":
     print("\n=================== EXTRATO ===================")
