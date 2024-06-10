5084f2ab36713dd055d9b8eedd0ea667b1b6b49e
Hyago Wendel <63081027+hyagow@users.noreply.github.com>
2024-06-09 22:51:03 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,7 +6,7 @@ import mysql.connector
 conexao = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="hsfHS2019@",
+    password="",
     database="myfirstdb"
 )
 cursor = conexao.cursor()
